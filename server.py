d35e2ad3e653ec721c7996e7c5d9258dadbf3586
aquaxp <aquaxp@users.noreply.github.com>
2016-09-26 14:14:11 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,18 @@
-from flask import Flask
+#!/usr/bin/env python
+"""Entry point for server"""
+
 from flask.ext.restful import reqparse, abort, Api, Resource
+from flask import Flask
 from api import *
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Auth, '/auth')
-#api.add_resource(Games, '/games')
 api.add_resource(Games, '/games/<string:user_id>')
 api.add_resource(Control, '/c2c')
 api.add_resource(Hello, '/')
 api.add_resource(State, '/state')
-#api.add_resource(Control, '/c2c')
-
-
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    pass
+    app.run(debug=False)
